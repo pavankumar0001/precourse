68658111e9521fafe09f9334dfa94b3eb305c680
PAVAN KUMAR <44501966+pavankumar0001@users.noreply.github.com>
2022-01-26 13:06:03 +0530

    25 jan assingment

---
+++ hello2.js
@@ -0,0 +1,11 @@
+var a = 10;
+var b = 2;
+var c = 13;
+
+if(a > b && a > c){
+    console.log(a +" is greater");
+}else if(b > a && b > c){
+    console.log(b +" is greater");
+}else{
+    console.log(c + " is greater");
+}
