5f2dea7bfe045de874e79b11bafb3a1ba270cbe7
PAVAN KUMAR <44501966+pavankumar0001@users.noreply.github.com>
2022-01-27 14:38:50 +0530

    26jan assignment

---
+++ 26jan assignment/findvowel.js
@@ -0,0 +1,10 @@
+var Char =prompt("enter a character")
+if(Char=="a"||Char=="A"||Char=="e"||Char=="E"||Char=="i"||Char=="I"||Char=="o"||Char=="O"||Char=="u"||Char=="U")
+{
+alert("character is vowel")
+}
+else
+{
+alert("character is not vowel")
+}
+
---
+++ 26jan assignment/minimum.js
@@ -0,0 +1,26 @@
+
+var A=prompt("enter first number")
+var B=prompt("enter second number")
+var C=prompt("enter third number")
+var a=parseInt(A)
+var b=parseInt(B)
+var c=parseInt(C)
+if(a<b && a<c)
+{
+alert(a +" is smallest number")
+}
+else if(b<a && b<c)
+{
+alert(b +" is smallest number")
+}
+else
+{
+alert(c +" is smallest number")
+}
+ 
+
+
+
+
+
+
